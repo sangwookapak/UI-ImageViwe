7f9fe09b866c88029e39ad5d522c4888754fdda9
dit05 <dit@dit05ui-imac.local>
2019-03-28 00:10:31 -0700

    3/28

--- UIImageViwe/ViewController.swift
+++ UIImageViwe/ViewController.swift
@@ -14,25 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var countLabel: UILabel!
     
     var count = 1
+    var cheak = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        ImageViwe.image = UIImage(named: "frame1.png")
-        //countlb.text = String(count)
-        countLabel.text = "frame\(count).png"
+       ImageViwe.image = UIImage(named: "frame1.png")
+       countLabel.text = "frame\(count).png"
     }
 
     @IBAction func btn1(_ sender: Any) {
         
-       count+=1
-        if(count > 5){
-            count = 1
+        if (count == 5) {
+            cheak = false
+        }else if (count == 1){
+            cheak = true
         }
+        if(cheak == true){
+            count += 1
+        }else if(cheak == false){
+            count -= 1
+        }
+        
         ImageViwe.image = UIImage(named:"frame\(count).png")
         countLabel.text = "frame\(count).png"
         
-        //image
         
     }
     
