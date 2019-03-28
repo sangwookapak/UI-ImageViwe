8a68829571ef8f6897838fd57b6a4d20050805ca
dit05 <dit@dit05ui-imac.local>
2019-03-27 23:30:24 -0700

    3/28

--- UIImageViwe/ViewController.swift
+++ UIImageViwe/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imvi: UIImageView!
+    @IBOutlet weak var countlb: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imvi.image = UIImage(named: "frame1.png")
+        //countlb.text = String(count)
+        countlb.text = "frame\(count).png"
     }
 
-
+    @IBAction func btn1(_ sender: Any) {
+        
+       count+=1
+        if(count > 5){
+            count = 1
+        }
+        imvi.image = UIImage(named:"frame\(count).png")
+        countlb.text = "frame\(count).png"
+        
+    }
+    
 }
 
